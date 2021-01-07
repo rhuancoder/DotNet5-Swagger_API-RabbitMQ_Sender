ceccb4ccd77461e96d61e8a2f172b9d33fdfb085
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2021-01-07 16:28:24 -0300

    Starting Web API;

---
+++ DotNet5-Swagger_API-RabbitMQ_Sender/DotNet5-Swagger_API-RabbitMQ_Sender.WebAPI/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet5_Swagger_API_RabbitMQ_Sender.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
